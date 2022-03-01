0bf8c3311716f3a8e9bcae7fe18e28d0a65ed7e0
alaminsahed <alaminsahed97@gmail.com>
2022-03-01 21:23:08 +0600

    feat: thumbnail done

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Result from '../components/Result';
+import requests from '../utilits/request'
 
 
-export default function Home() {
+export default function Home({ results }) {
+
   return (
     <div>
       <Head>
@@ -12,6 +15,23 @@ export default function Home() {
       </Head>
       <Header />
       <Nav />
+      <Result results={results} />
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
